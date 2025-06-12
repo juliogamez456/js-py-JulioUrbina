15899323ccc62271681cd6c2af03762502bb762f
Julio <juliofernandourbinagamez@gmail.com>
2025-06-12 13:46:20 -0600

    Ejercicio2 en javascript completado

---
+++ ejercicios-js/ejercicio2.js
@@ -0,0 +1,14 @@
+// 2.** Verificador de par/iepar**
+// Genera un número y determina si est par o impar. 
+// Genera un número aleatorio entre 1 y 100
+ const numero= Math.floor(Math.random()*100) + 1;
+
+console.log('numero generado: ${numero}');
+
+if (numero % 2===0) {
+console.log("$(numero) es par.");
+process.exit(0); // Salida exitosa
+} else {
+ console.log("$(numero) es impar.");
+process,exit(0); // Salida exitosa
+}
