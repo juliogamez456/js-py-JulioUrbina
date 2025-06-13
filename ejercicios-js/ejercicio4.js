857d65f43873d0ee7850061b3662d598f1ef9efe
Julio <juliofernandourbinagamez@gmail.com>
2025-06-12 21:48:47 -0600

    ejercicio 4 en javascript completado

---
+++ ejercicios-js/ejercicio4.js
@@ -0,0 +1,9 @@
+// Generar tabla de multiplicar del 1 al 10
+for (let i = 1; i <= 10; i++) {
+  console.log(`Tabla del ${i}:`);
+  for (let j = 1; j <= 10; j++) {
+    console.log(`${i} x ${j} = ${i * j}`);
+  }
+  console.log('--------------------');
+}
+ 
